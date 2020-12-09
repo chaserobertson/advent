81b7c6e41eb8fa6c1140646fef25f5174ae6d5e9
Chase Robertson <crobertson@crobertson.C02YL0HHLVCH>
2020-12-08 21:16:56 -0700

    day one complete

--- 01/01.py
+++ 01/01.py
@@ -1,7 +1,23 @@
-with open('input.txt') as input:
-    output = 0
+import pathlib
+in_file = pathlib.Path.cwd().joinpath('01', 'input.txt')
+
+TARGET = 2020
+
+with open(in_file) as input:
     lines = input.readlines()
-    for line in lines:
-        if line == 2020:
-            output = line
-    print(output)
+
+for i in range(0, len(lines)):
+    for j in range(i, len(lines)):
+        li = int(lines[i])
+        lj = int(lines[j])
+        if li + lj == TARGET:
+            print(li * lj)
+
+for i in range(0, len(lines)):
+    for j in range(i, len(lines)):
+        for k in range(j, len(lines)):
+            li = int(lines[i])
+            lj = int(lines[j])
+            lk = int(lines[k])
+            if li + lj + lk == TARGET:
+                print(li * lj * lk)
