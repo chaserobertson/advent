bed09e2f2dcf73ddf1a24ceb7545286a5483782e
Chase Robertson <chaserobertson208@gmail.com>
2020-12-16 19:05:37 -0500

    days 14 & 15 pt 1

--- 14/sol.py
+++ 14/sol.py
@@ -1,7 +1,64 @@
 import pathlib
-import re
+import math
 
-in_file = pathlib.Path.cwd().joinpath('', 'smol_input.txt')
+in_file = pathlib.Path.cwd().joinpath('14', 'input.txt')
 
 with open(in_file) as input:
     lines = input.readlines()
+
+MASK = 'mask'
+MEM = 'mem'
+MAX_INT = 68719476735
+
+# for XXXX0XX mask, AND 1111011 (X 0's will stay 0, X 1's will stay 1, 0 will be 0 regardless)
+# for XXXX1XX mask, OR 00001100 (X 0's will stay 0, X 1's will stay 1, 1 will be 1 regardless)
+
+
+class Mask():
+    def __init__(self, val):
+        self.masks_1 = []
+        self.masks_0 = []
+
+        for i in range(0, len(val)):
+            char = val[i]
+            place = math.pow(2, 35 - i)
+            if char == '1':
+                self.masks_1.append(int(place))
+            elif char == '0':
+                self.masks_0.append(MAX_INT - int(place))
+            # else do nothing
+
+    def __str__(self):
+        output = '1 masks: '
+        for mask in self.masks_1:
+            output += str(bin(mask)) + ' '
+        output += '\n0 masks: '
+        for mask in self.masks_0:
+            output += str(bin(mask)) + ' '
+        return output
+        # return str(self.masks_0) + '\n' + str(self.masks_1)
+
+    def apply(self, value):
+        new_value = value
+        for mask in self.masks_0:
+            new_value = new_value.__and__(mask)
+        for mask in self.masks_1:
+            new_value = new_value.__or__(mask)
+        return new_value
+
+
+mem = {}
+masks = []
+
+for line in lines:
+    components = line.split(' = ')
+    cmd = components[0]
+    val = components[1]
+    if cmd == MASK:
+        masks.append(Mask(val))
+    elif cmd[:3] == MEM:
+        mem_loc = cmd.split('[')[1].split(']')[0]
+        mem[mem_loc] = masks[-1].apply(int(val))
+
+print('part 1')
+print(sum(mem.values()))
--- 15/sol.py
+++ 15/sol.py
@@ -1,7 +1,33 @@
 import pathlib
 import re
 
-in_file = pathlib.Path.cwd().joinpath('', 'smol_input.txt')
+in_file = pathlib.Path.cwd().joinpath('15', 'input.txt')
 
 with open(in_file) as input:
     lines = input.readlines()
+
+FIN = 2020
+
+starting_numbers = lines[0].strip().split(',')
+numbers = [int(num) for num in starting_numbers]
+print('Starting numbers: ' + str(numbers))
+
+turns = {}
+for i in range(0, len(numbers)):
+    turns[numbers[i]] = [i+1]
+
+start = len(numbers) + 1
+for turn in range(start, FIN + 1):
+    last_spoken = numbers[-1]
+    turns_spoken = turns.setdefault(last_spoken, [])
+    if len(turns_spoken) > 1:
+        next_digit = turns_spoken[-1] - turns_spoken[-2]
+        numbers.append(next_digit)
+        digit_turns = turns.setdefault(next_digit, [])
+        digit_turns.append(turn)
+    else:
+        numbers.append(0)
+        digit = turns.setdefault(0, [])
+        digit.append(turn)
+
+print('The ' + str(FIN) + 'th number is: ' + str(numbers[-1]))
