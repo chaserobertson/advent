85958f33cae9dc89d4b5f2c41c0cd79f86a51b69
Chase <chaserobertson208@gmail.com>
2022-12-04 21:02:56 +1300

    day4

--- 2022/04/1.py
+++ 2022/04/1.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
 
 input = []
-with open('2022/0/test.txt', 'r') as f:
+with open('2022/04/input.txt', 'r') as f:
     input = f.read().splitlines()
 
-print(input)
+contains = 0
+overlaps = 0
+
+for line in input:
+    ranges = line.split(",")
+    edges = ranges[0].split('-') + ranges[1].split('-')
+    edges = [int(x) for x in edges]
+
+    if edges[0] >= edges[2] and edges[1] <= edges[3] \
+        or edges[0] <= edges[2] and edges[1] >= edges[3]:
+        contains += 1
+
+    if edges[2] <= edges[0] <= edges[3] \
+        or edges[2] <= edges[1] <= edges[3] \
+        or edges[0] <= edges[2] <= edges[1] \
+        or edges[0] <= edges[3] <= edges[1]:
+        overlaps += 1
+
+print(contains)
+print(overlaps)
