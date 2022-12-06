aec7e4dbe0fd74ceb122fba697023a888d551edb
Chase <chaserobertson208@gmail.com>
2022-12-06 18:10:39 +1300

    day6

--- 2022/06/1.py
+++ 2022/06/1.py
@@ -8,8 +8,7 @@ def first_distinct_set(lines, set_len):
     for line in lines:
         for i in range(set_len, len(line)):
             if len(set(line[i-set_len:i])) == set_len:
-                print(i)
-                break
+                return i
 
-first_distinct_set(input, 4)
-first_distinct_set(input, 14)
+print(first_distinct_set(input, 4))
+print(first_distinct_set(input, 14))
